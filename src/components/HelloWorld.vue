9183f019182a467bc422b2cbb800773657194572
ShaneLee-9 <lixuewen1995@outlook.com>
2022-11-18 11:17:30 +0800

    test plugin

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,13 +29,14 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+<script setup>
+defineOptions({
+  name: 'HelloNewWorld'
+})
+
+defineProps({
+  msg: String
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      require('unplugin-vue-define-options/webpack')({
+        include: [/\.vue$/, /\.vue\?vue/]
+      })
+    ]
+  }
 })
